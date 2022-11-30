aa5825bb041e3812f508049167d44f1626a867ec
Юлия Сенина <JuLKa9449@yandex.ru>
2022-11-30 15:26:07 +0300

    commit

--- server.js
+++ server.js
@@ -32,14 +32,7 @@ app.use(
   })
 );
 
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-    "Access-Control-Allow-Origin": true,
-    allowMethods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
+app.use(cors());
 
 const router = new Router();
 
